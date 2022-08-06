1e9d7eb85f4f403787b8efb5165e5c44c0702275
Biplab Wagle <biplabwagle@gmail.com>
2022-08-06 13:58:25 -0500

    Import PageLayouts to a single component and adds it to the named export for easier access

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+};
+
+// exposure to foreign culture helps them to adapt quickly and respect traditions and beliefs that might not necessarily align with their own.
