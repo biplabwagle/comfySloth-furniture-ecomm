0eed63b93afc3461073fdb5c81e5ed63948038b6
Biplab Wagle <biplabwagle@gmail.com>
2022-08-06 13:59:03 -0500

    Added pages to the app and react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/products/:id" element={<SingleProduct />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
