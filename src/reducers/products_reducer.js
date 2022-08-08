bdf7a586dba1247112da0dbfcce484b89be0b68e
Biplab Wagle <biplabwagle@gmail.com>
2022-08-08 11:34:15 -0500

    Added api calls and state  for singleproduct page

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -27,6 +27,24 @@ const products_reducer = (state, action) => {
       };
     case GET_PRODUCTS_ERROR:
       return { ...state, products_loading: false, products_error: true };
+    case GET_SINGLE_PRODUCT_BEGIN:
+      return {
+        ...state,
+        single_product_loading: true,
+        single_product_error: false,
+      };
+    case GET_PRODUCTS_ERROR:
+      return {
+        ...state,
+        single_product_error: true,
+        single_product_loading: false,
+      };
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product: action.payload,
+      };
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
