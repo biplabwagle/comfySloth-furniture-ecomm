2434071ef3fb40f0df380f3cb4d08af68b6ac3a3
Biplab Wagle <biplabwagle@gmail.com>
2022-08-15 09:25:20 -0500

    Added Private Route to Checkout Page

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,10 +1,20 @@
-import React, { Children } from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 // will remove later
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({ children }) => {
-  return children;
+  const { userClient } = useUserContext();
+  /*
+   *if user client does exists, which only happens after auth0
+   *authentication, then return children which in this case is
+   *checkout page
+   */
+  if (userClient) {
+    return children;
+  }
+  //otherwise return the user to the landing page
+  return <Navigate to="/" redirect={true} />;
 };
 export default PrivateRoute;
